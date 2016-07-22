efe61022256a67ce1b24ea4ca4e75fa8663ca6ba
Daian Martinho <daian.m@hotmail.com>
2016-07-22 16:02:56 -0300

    [slider] grade + mask + js
    tempo de estudo/execução 4 horas

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+ 
+    //slider init
+    $("#slides").responsiveSlides({
+        speed: 700,     //Speed of the transition
+        timeout: 3500,  //Time between slide transitions
+        pager: false     //Show the pagination
+    });
+    
+    
+});
